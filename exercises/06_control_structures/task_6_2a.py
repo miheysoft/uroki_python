1abc6fa4e0c97e6389891f347616c94463bfdbf2
it_50 <skbest@yandex.ru>
2023-11-12 18:03:43 +0700

    решил 6_2b

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,27 +17,47 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Введите IP-адрес ')
+ip_okt = ip.split('.')
+valid_ip = len(ip_okt) == 4
 
-ip = input('Введите IP-адрес ').split('.')
-
-valid_ip = len(ip) == 4
-
-for i in ip:
+for i in ip_okt:
     valid_ip = i.isdigit() and 0 <= int(i) <= 255 and valid_ip
 
 if valid_ip:
-   ip_int = int(ip[0]) 
-   if ip_int >= 1 and ip_int <= 223:
-      type_adr = 'unicast'
-   elif ip_int >= 224 and ip_int <= 239:
-      type_adr = 'multicast'
-   elif ip == "255.255.255.255":
-      type_adr = 'local broadcast'
-   elif ip == "0.0.0.0":
-      type_adr = 'unassigned'
-   else:
-      type_adr = 'unused'
-   print(f"{type_adr}")
+    ip_int = int(ip_okt[0]) 
+    if ip_int >= 1 and ip_int <= 223:
+        type_adr = 'unicast'
+    elif ip_int >= 224 and ip_int <= 239:
+        type_adr = 'multicast'
+    elif ip == "255.255.255.255":
+        type_adr = 'local broadcast'
+    elif ip == "0.0.0.0":
+        type_adr = 'unassigned'
+    else:
+        type_adr = 'unused'
+    print(f"{type_adr}")
    
 else:
-   print('Неправильный IP-адрес')
+    print('Неправильный IP-адрес')
+
+#ip = input("Введите IP-адрес в формате x.x.x.x: ")
+# octets = ip
+# valid_ip = len(octets) == 4
+
+# for i in octets:
+#     valid_ip = i.isdigit() and 0 <= int(i) <= 255 and valid_ip
+
+# if valid_ip:
+#     if 1 <= int(octets[0]) <= 223:
+#         print("unicast")
+#     elif 224 <= int(octets[0]) <= 239:
+#         print("multicast")
+#     elif ip == "255.255.255.255":
+#         print("local broadcast")
+#     elif ip == "0.0.0.0":
+#         print("unassigned")
+#     else:
+#         print("unused")
+# else:
+#     print("Неправильный IP-адрес")
--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,53 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+# while True:
+#     ip = input("Введите IP-адрес в формате x.x.x.x: ")
+#     octets = ip.split(".")
+#     valid_ip = len(octets) == 4
+
+#     for i in octets:
+#         valid_ip = i.isdigit() and 0 <= int(i) <= 255 and valid_ip
+
+#     if valid_ip:
+#         break
+#     print("Неправильный IP-адрес")
+
+# if 1 <= int(octets[0]) <= 223:
+#     print("unicast")
+# elif 224 <= int(octets[0]) <= 239:
+#     print("multicast")
+# elif ip == "255.255.255.255":
+#     print("local broadcast")
+# elif ip == "0.0.0.0":
+#     print("unassigned")
+# else:
+#     print("unused")
+
+while True:
+    ip = input('Введите IP-адрес ')
+    ip_okt = ip.split('.')
+    valid_ip = len(ip_okt) == 4
+
+    for i in ip_okt:
+        valid_ip = i.isdigit() and 0 <= int(i) <= 255 and valid_ip
+    
+    if valid_ip:
+        break
+    else:
+        print('Неправильный IP-адрес')    
+
+if valid_ip:
+    ip_int = int(ip_okt[0]) 
+    if ip_int >= 1 and ip_int <= 223:
+        type_adr = 'unicast'
+    elif ip_int >= 224 and ip_int <= 239:
+        type_adr = 'multicast'
+    elif ip == "255.255.255.255":
+        type_adr = 'local broadcast'
+    elif ip == "0.0.0.0":
+        type_adr = 'unassigned'
+    else:
+        type_adr = 'unused'
+    print(f"{type_adr}")
+   
