73e9b25379410e40bb49a0fdd8746dfbfcadf38e
it_50 <skbest@yandex.ru>
2024-02-04 06:04:37 +0700

    слава Богу продолжил изуч. Питон

--- exercises/07_files/task_7_1.py
+++ exercises/07_files/task_7_1.py
@@ -19,9 +19,5 @@ result = {}
 with open('ospf.txt') as f:
     for line in f:
         line_list = line.split()
-        if line_list and line_list[1][0].isdigit():
-            interface = line_list[0]
-            address = line_list[1]
-            result[interface] = address
-
-print(result)
+        print(line_list)
+        
