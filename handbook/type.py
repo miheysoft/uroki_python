3c2474f6fb8c4f2ee157363dca88a05e07d14222
it_50 <skbest@yandex.ru>
2024-04-29 16:28:58 +0700

    изучил числа

--- handbook/type.py
+++ handbook/type.py
@@ -11,3 +11,18 @@ print(my_dict)
 # множество
 my_set = {1, 2, "дурень", 6.1}
 print(my_set)
+
+# числа 
+integer_t = 10
+float_t = 3.1
+complex_t = 2 + 3j
+
+# выводим результат
+print(type(integer_t))
+print(type(float_t))
+print(type(complex_t))
+
+# арифметические операции
+print(f"возведение в степень {integer_t ** float_t}") # 
+print(f"целочисленноеделение {integer_t // float_t}") # 
+print(f"остаток от деления {integer_t % float_t}") # 
