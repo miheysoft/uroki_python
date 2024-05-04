720ccb0d1b3d9a81cc086bf4036ae8926cf2be10
it_50 <skbest@yandex.ru>
2024-05-04 14:24:25 +0700

    прошел списки кортежы

--- handbook/list.py
+++ handbook/list.py
@@ -11,3 +11,12 @@ print(my_list)
 combo_list = my_list + my_listtws
 print(combo_list)
 # Методы
+ovoshi_list = ['помидоры', 'огурцы', 'баклажаны']
+ovoshi_list.append('морковка')
+ovoshi_list.insert(0, 'лук')
+del_ovoshi = ovoshi_list.pop() # удаляет последний элемент из списка и возвращает его значение
+print(ovoshi_list)
+print (del_ovoshi)
+ovoshi_list.remove('огурцы') # remove() используется для удаления первого вхождения элемента из списка. Если элемент не найден, функция возвращает None.
+ovoshi_list.sort() # сортирует список по возрастанию значений элементов
+print(ovoshi_list)
---
+++ handbook/tuple.py
@@ -0,0 +1,6 @@
+tuple_int = (1, 2, 3)
+tuple_null = tuple('работа по найму')
+tuple_list = (4, True, '1')
+print(tuple_int[2])
+print(tuple_null[-1])
+print(tuple_list[0:1]) # получается список
