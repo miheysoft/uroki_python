7d3048e1e84904e4da60a46fd99893417da666c4
it_50 <skbest@yandex.ru>
2024-05-18 18:21:44 +0700

    начал множество

---
+++ handbook/set.py
@@ -0,0 +1,9 @@
+my_set = {1, 2, 3}
+print(my_set)
+my_set1 = set(['1', '2', '3', '4']) # 
+print(my_set1)
+my_set.add('5')#add element to sets
+print(my_set)
+my_set.remove(1) # удаляет элемент по индексу 
+print(f"удалил элемент 1 {my_set}") #
+
--- handbook/tuple.py
+++ handbook/tuple.py
@@ -1,6 +1,11 @@
+# создание кортежа 
 tuple_int = (1, 2, 3)
 tuple_null = tuple('работа по найму')
 tuple_list = (4, True, '1')
 print(tuple_int[2])
 print(tuple_null[-1])
 print(tuple_list[0:1]) # получается список
+# методы кортежа 
+owochi = ('a', 'a', 'b', 'a')
+print(owochi.count('a')) # количество вхождений
+print(owochi.index('a')) # индекс первого вхождения (возвращает индекс)
