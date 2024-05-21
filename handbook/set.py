b7cb1daf05e67a6007dc016146c17352ef4b159f
it_50 <skbest@yandex.ru>
2024-05-21 11:26:02 +0700

    разбираюсь со множеством

--- handbook/set.py
+++ handbook/set.py
@@ -1,9 +1,18 @@
 my_set = {1, 2, 3}
 print(my_set)
-my_set1 = set(['1', '2', '3', '4']) # 
+my_set1 = set(["1", "2", "3", "4"])  #
 print(my_set1)
-my_set.add('5')#add element to sets
+my_set.add("5")  # add element to sets
 print(my_set)
-my_set.remove(1) # удаляет элемент по индексу 
-print(f"удалил элемент 1 {my_set}") #
+my_set.remove(1)  # удаляет элемент по индексу
+print(f"удалил элемент 1 {my_set}")  # удаляет элемент по индексу
 
+other_set = {1, 2, 3}
+union_set = my_set.union(other_set)  # объединяет два множества
+print(union_set)
+
+intersection_set = my_set.intersection(other_set)  # объединяет два множества
+print(intersection_set)
+
+defference_set = my_set.difference(other_set)  #
+print(defference_set)
