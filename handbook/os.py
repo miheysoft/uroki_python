49e447b0c5aac4b795f6783a0ce470f2e7a91162
it_50 <skbest@yandex.ru>
2024-06-27 18:57:23 +0700

    Как работать с модулем OS

--- handbook/os.py
+++ handbook/os.py
@@ -1,16 +1,23 @@
 import os
 
-files = os.listdir(".")
-print(files)
-os.system("ls -l")
-# получение значений переменных окружения
-print(os.environ["HOME"])
-print(os.environ["PATH"])
-print(os.environ["SHELL"])
+# files = os.listdir(".")
+# print(files)
+# os.system("ls -l")
+# # получение значений переменных окружения
+# print(os.environ["HOME"])
+# print(os.environ["PATH"])
+# print(os.environ["SHELL"])
 
-# Получение всех переменных окружения
-env_variables = os.environ
+# # Получение всех переменных окружения
+# env_variables = os.environ
 
-# Вывод всех переменных окружения
-for key, value in env_variables.items():
-    print(f"{key}: {value}")
+# # Вывод всех переменных окружения
+# for key, value in env_variables.items():
+#     print(f"{key}: {value}")
+
+for rooot, dirs, files in os.walk("/home/miheev/"):
+    print(rooot)
+    for d_ir in dirs:
+        print(d_ir)
+    for f_ile in files:
+        print(f_ile)
