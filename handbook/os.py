e74faa0b32925743ad3e8bd43aa61e233d85d4a3
it_50 <skbest@yandex.ru>
2024-06-29 21:34:41 +0700

    изучаю модуль os

--- handbook/os.py
+++ handbook/os.py
@@ -1,22 +1,22 @@
 import os
 
-# files = os.listdir(".")
-# print(files)
-# os.system("ls -l")
-# # получение значений переменных окружения
-# print(os.environ["HOME"])
-# print(os.environ["PATH"])
-# print(os.environ["SHELL"])
+files = os.listdir(".")
+print(files)
+os.system("ls -l")
+# получение значений переменных окружения
+print(os.environ["HOME"])
+print(os.environ["PATH"])
+print(os.environ["SHELL"])
 
-# # Получение всех переменных окружения
-# env_variables = os.environ
+# Получение всех переменных окружения
+env_variables = os.environ
 
-# # Вывод всех переменных окружения
-# for key, value in env_variables.items():
-#     print(f"{key}: {value}")
+# Вывод всех переменных окружения
+for key, value in env_variables.items():
+    print(f"{key}: {value}")
 
-for rooot, dirs, files in os.walk("/home/miheev/"):
-    print(rooot)
+for root, dirs, files in os.walk("/home/miheev/"):
+    print(root)
     for d_ir in dirs:
         print(d_ir)
     for f_ile in files:
